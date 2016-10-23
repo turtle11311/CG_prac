7b8cd5fdf6d6f7539a3cf46c57a645e159c56030
TurtleBee <turtle11311@gmail.com>
2016-10-23 13:31:20 +0800

    Makefile add run target remove trailing space

--- Makefile
+++ Makefile
@@ -2,11 +2,17 @@ CC = gcc
 CXX = g++
 TARGET = CG_EXE
 LDFLAGS += -lGL -lGLU -lglut -lX11
+FILE = ${file}
 
-.PHONY: all
+.PHONY: all clean run
 
 all: $(TARGET)
 	./$(TARGET)
 
-$(TARGET): ${file}
+$(TARGET): $(FILE)
 	$(CXX) $< -o $(TARGET) $(LDFLAGS)
+
+run: clean all
+
+clean:
+	$(RM) $(TARGET)
--- rotate.cpp
+++ rotate.cpp
@@ -9,7 +9,7 @@
 
 const size_t POINTNUM = 4;
 const size_t WIN_WIDTH = 800;
-const size_t WIN_HEIGHT = 800;
+const size_t WIN_HEIGHT = 600;
 
 float theta = 45.0;      // rotation angel of graph
 
@@ -19,11 +19,11 @@ GLfloat vertices[POINTNUM][3] = {{-1.0,-1.0},{1.0,-1.0}, {1.0,1.0}, {-1.0,1.0}};
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glRotatef(theta, 0.0, 0.0, 1.0);
-    
+
     glBegin(GL_POLYGON);
         glColor3f(0.5, 0.5, 1.0);
         for (size_t i = 0; i < POINTNUM; ++i) {
@@ -53,17 +53,17 @@ int main(int argc, char **argv)
     Window wid = DefaultRootWindow(pdsp);
     XWindowAttributes xwAttr;
     XGetWindowAttributes(pdsp, wid, &xwAttr);
-    
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition((xwAttr.width - WIN_WIDTH)/ 2,
                            (xwAttr.height - WIN_HEIGHT) / 2);
     glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
     glutCreateWindow("Color");
-    
+
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutTimerFunc(10, rotate_it, 1);
-    
+
     glutMainLoop();
 }
